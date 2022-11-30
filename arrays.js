8a15fd09b8fd8c01c1448061be124ba85427a61a
Laura Raellen <lauraraellen0@gmail.com>
2022-11-30 20:56:12 -0300

    Entendendo na prática como se funciona um array e suas propriedades.

---
+++ arrays.js
@@ -0,0 +1,18 @@
+/*
+ *Array, valor ou arranjo, é uma estrutura ampliada atilizada 
+ * e implementada em quase em todas as lingugens de programação
+ */
+
+['a', 10, 'b', true] // total de 4 elementos
+
+
+/**
+ * Os arrays são dinãmicos
+ * 
+ * você poderá ter dados de diferentes tipos misturados dentro
+ * de um array.Strings, numbers, booleans,objetos, funções,
+ * e até outros arrays.
+ * 
+ * existem muitos métodos já implementados   
+ * \/push()\ pop()\ find()\ filter()\/
+ */
