8ad6c28f1e4abb7e9b933614caf05328f1d642d5
Laura Raellen <89939694+LauraRaellen@users.noreply.github.com>
2022-12-14 19:46:57 -0300

    Add files via upload
    fazendo na pratica como que funciona o conceito de stack e queue por baixo dos panos.

--- stack.js
+++ stack.js
@@ -13,7 +13,46 @@
  *      LIFO: Last In First Out
  *  O último elemento a entrar na pilha, aquele elemento
  *  do topo da pilha, é o primeiro a sair 
- * 
- * MÉTODOS FUNDAMENTAIS
- *      
+ *  
  */
+
+ // Passo 1: Modelando
+ class Stack {
+  constructor() {
+    this.data = []
+    this.top = -1
+  }
+  push(value) {
+    this.top++
+    this.data[this.top] = value
+    return this.data
+  }
+
+  pop() {
+    if (this.top < 0) return undefined
+    const poppedTop = this.data[this.top]
+    delete this.data[this.top]
+    this.top--
+    return poppedTop
+  }
+
+  peek() {
+    return this.top >= 0 ? this.data[this.top] : undefined
+  }
+ }
+
+ // Passo 2: utilizando
+ const stack = new Stack()
+
+ // adicionar dados
+ stack.push('learning');
+ stack.push('data');
+ console.log(stack.push('structures'));
+
+ console.log(stack.peek());
+
+ // remover
+ stack.pop()
+ console.log(stack.pop());
+
+ console.log(stack.peek());
